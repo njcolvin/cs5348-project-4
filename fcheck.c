f5e286833d258d9d364cc8ff68383d9db735c4e0
Nick Colvin <nickjcolvin@gmail.com>
2022-12-14 21:26:56 -0500

    rule 5

--- fcheck.c
+++ fcheck.c
@@ -24,8 +24,6 @@ main(int argc, char *argv[])
   struct superblock *sb;
   struct dirent *de;
   struct stat *st;
-  char *bmp;
-  // uint *bitties;
 
   if(argc < 2){
     fprintf(stderr, "Usage: fcheck <file_system_image>\n");
@@ -93,9 +91,13 @@ main(int argc, char *argv[])
       
       for (j = 0; j < NDIRECT; j++) {
         index = i * (NDIRECT + 1) + j;
-        direct_blocks[index] = dip[i + 1].addrs[j];
-        if (direct_blocks[index] > 0)
+        if (dip[i + 1].addrs[j] > 0) {
+          direct_blocks[index] = dip[i + 1].addrs[j];
           printf("inode %d direct block %d = %d\n", i + 1, j, direct_blocks[index]);
+        } else {
+          direct_blocks[index] = 0;
+          printf("inode %d direct block %d unused.\n", i + 1, j);
+        }
       }
 
       // get indirect blocks
@@ -114,24 +116,16 @@ main(int argc, char *argv[])
         if (indirect_blocks[index] > 0)
           printf("inode %d indirect block %d = %d\n", i + 1, j, indirect_blocks[index]);
       }
+    } else {
+      for (j = 0; j < NDIRECT; j++) {
+        index = i * (NDIRECT + 1) + j;
+        direct_blocks[index] = 0;
+      }
     }
     
   }
 
-  // check inode types
-  for (i = 1; i <= sb->ninodes; i++) {
-    if (inode_types[i - 1] < 0 || inode_types[i - 1] > 3) {
-      fprintf(stderr, "ERROR: bad inode.\n");
-    }
-  }
-    
-
-  // get the bitmap block
-  bmp = (addr + sb->nblocks * BLOCK_SIZE);
-  printf("bmp=%p\n", bmp);
-  printf("BPB * sb->size=%d\n", BPB * sb->size);
-  
-
+  // get blocks in use in bitmap
   int bits_in_use[BPB * (sb->size/BPB) + (sb->size%BPB)];
   char *bp;
   int b, bi;
@@ -140,23 +134,35 @@ main(int argc, char *argv[])
     bp = addr + BBLOCK(b, sb->ninodes) * BLOCK_SIZE;
     for(bi = 0; bi < BPB; bi++){
       int m = 1 << (bi % 8);
-      if(!((uchar)*(bp + bi/8) & m) == 0){  // Is block free?
-        printf("bit %d in block %d in use\n", b + bi, b / BPB);
+      if(((uchar)*(bp + bi/8) & m) != 0){  // Is block in use?
         bits_in_use[b + bi] = 1;
       } else if (b + bi < sb->size)
         bits_in_use[b + bi] = 0;
     }
   }
 
-  int count = 0;
-  for(b = 0; b < sb->size; b++) {
-    if (bits_in_use[b] == 1) {
-      count++;
+  // check inode types
+  for (i = 1; i <= sb->ninodes; i++) {
+    if (inode_types[i - 1] < 0 || inode_types[i - 1] > 3) {
+      fprintf(stderr, "ERROR: bad inode.\n");
+      exit(1);
     }
   }
 
-  printf("%d bits set\n", count);
-  // *(addr + ((i/8) * sizeof(char))) & m) == 0
+  // check if direct blocks are marked in use in bitmap
+  for (i = 1; i < sb->ninodes; i++) {
+    for (j = 0; j < NDIRECT; j++) {
+      index = i * (NDIRECT + 1) + j;
+      if (direct_blocks[index] > 0) {
+        printf("inode %d direct block %d = %d\n", i + 1, j, direct_blocks[index]);
+        if (bits_in_use[direct_blocks[i]] == 0) {
+          fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
+          exit(1);
+        }
+      }
+    }
+  }
+
   exit(0);
 
 }
